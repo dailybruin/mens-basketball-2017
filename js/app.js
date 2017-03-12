e1e5a6273d4d83911d7d5a3d3df6029da635b86a
Leo <siow98@ucla.edu>
2017-03-12 11:26:36 -0700

    Timeline

--- js/app.js
+++ js/app.js
@@ -62,3 +62,11 @@ $(".flip-toggle").on("click", function () {
   console.log(playerName);
   $("#" + playerCard).flip("toggle");
 });
+
+
+ $(".timeline-item").hover(function () {
+  $(".timeline-item").removeClass("active");
+  $(this).toggleClass("active");
+  $(this).prev(".timeline-item").toggleClass("close");
+  $(this).next(".timeline-item").toggleClass("close");
+            });

f1dac5f0ca46dc8bf83478e67e1220b39676045b
Leo <siow98@ucla.edu>
2017-03-14 13:05:49 -0700

    Centered timeline, scroll down and fades in

--- js/app.js
+++ js/app.js
@@ -63,10 +63,62 @@ $(".flip-toggle").on("click", function () {
   $("#" + playerCard).flip("toggle");
 });
 
-
+/*
  $(".timeline-item").hover(function () {
   $(".timeline-item").removeClass("active");
   $(this).toggleClass("active");
   $(this).prev(".timeline-item").toggleClass("close");
   $(this).next(".timeline-item").toggleClass("close");
             });
+
+*/
+
+$(document).ready(function() {
+    
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+    
+        /* Check the location of each desired element */
+        $('.fade').each( function(i){
+            
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},500);
+                    
+            }
+            
+        }); 
+    
+    });
+    
+});
+
+
+/*
+
+    function showImages(el) {
+        var windowHeight = jQuery( window ).height();
+        $(el).each(function(){
+            var thisPos = $(this).offset().top;
+
+            var topOfWindow = $(window).scrollTop();
+            if (topOfWindow + windowHeight - 200 > thisPos ) {
+                $(this).addClass("fadeIn");
+            }
+        });
+    }
+
+    // if the image in the window of browser when the page is loaded, show that image
+    $(document).ready(function(){
+            showImages('.timeline-item');
+    });
+
+    // if the image in the window of browser when scrolling the page, show that image
+    $(window).scroll(function() {
+            showImages('.timeline-item');
+    });
+*/
